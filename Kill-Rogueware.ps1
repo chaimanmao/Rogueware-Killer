1ca4c2bd6b5eeab66a9f107db1cba515c75d143c
Vizo <vizodream@outlook.com>
2017-05-22 23:00:33 +0800

    code refactoring
    half

--- Kill-Rogueware.ps1
+++ Kill-Rogueware.ps1
@@ -21,9 +21,9 @@ latest project:https://github.com/vizogood/Rogueware-Killer
 # check for permissions 
 $currentWi = [Security.Principal.WindowsIdentity]::GetCurrent()
 $currentWp = [Security.Principal.WindowsPrincipal]$currentWi
-if( -not $currentWp.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
-    $boundPara = ($MyInvocation.BoundParameters.Keys | foreach{
-        '-{0} {1}' -f  $_ ,$MyInvocation.BoundParameters[$_]} ) -join ' '
+if ( -not $currentWp.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    $boundPara = ($MyInvocation.BoundParameters.Keys | foreach {
+            '-{0} {1}' -f $_ , $MyInvocation.BoundParameters[$_]} ) -join ' '
     $currentFile = (Resolve-Path  $MyInvocation.InvocationName).Path
     $fullPara = $boundPara + ' ' + $args -join ' '
     Start-Process "$psHome\powershell.exe"   -ArgumentList "$currentFile $fullPara" -verb runas
@@ -35,86 +35,103 @@ Write-Host "any key to continue" -ForegroundColor Red -BackgroundColor White
 $PSScriptRoot
 $program = "C:\Program Files"
 $programx86 = "C:\Program Files (x86)"
-$appdata = Get-Childitem env:APPDATA | %{ $_.Value } 
+$appdata = Get-Childitem env:APPDATA | % { $_.Value } 
 
 # block digital certificate
-ForEach($file in Dir "$PSScriptRoot\Cer"){
-    certutil -f -addstore -user "Disallowed" "$PSScriptRoot\Cer\$file"
+try {
+    ForEach ($file in Dir "$PSScriptRoot\cer") {
+        certutil -f -addstore -user "Disallowed" "$PSScriptRoot\Cer\$file"
+    }
+}catch {
+    Write-Host "set Certificates limit failed ! " -ForegroundColor Red
+}Finally {
+    Write-Host "set Certificates limit successfully !  " -ForegroundColor Green
 }
-Write-Host "`n"
-Write-Host "set Certificates limit successfully " -ForegroundColor Green
 
 # set directory permissions
-Write-Host "`n"
 Write-Host "setting directory permission limit" -ForegroundColor Green
+
 # Baidu 
+$baiduDirectoryList= New-Object System.Collections.ArrayList
+for($line=0;$line -le (Get-Content "./directory-list/baidu-directory-list.txt").Length-1 ;$line+=1){
+    $baiduDirectoryList.add((Get-Content "./directory-list/baidu-directory-list.txt")[$line]) | Out-Null
+}
+
+for($i=0;$i -le $baiduDirectoryList.Count;$i+=1){
+    New-Item "$baiduDirectoryList[i]"  -ItemType Directory
+    icacls.exe "$baiduDirectoryList[i]" /deny Everyone:F
+}
+
 New-Item "$program\Baidu\BaiduAn"    -ItemType Directory
 New-Item "$program\Baidu\BaiduSd"    -ItemType Directory
 New-Item "$appdata\Baidu"            -ItemType Directory
 New-Item "$programx86\Baidu\BaiduAn" -ItemType Directory
 New-Item "$programx86\Baidu\BaiduSd" -ItemType Directory
-for($i = 1;$i -le 9;$i++ ){
-     for($j = 0;$j -le 9; $j++ ){
-         New-Item "$program\BaiduSd$i.$j" -ItemType Directory
-         attrib +s +h "$program\BaiduSd$i.$j"
-         cacls "$program\BaiduSd$i.$j" /e /d Everyone
-     }
+icacls.exe "$program\Baidu\BaiduAn" /deny Everyone:F
+icacls.exe "$program\Baidu\BaiduSd" /deny Everyone:F
+icacls.exe "$appdata\Baidu" /deny Everyone:F
+icacls.exe "$programx86\Baidu\BaiduAn" /deny Everyone:F
+icacls.exe "$programx86\Baidu\BaiduSd" /deny Everyone:F
+for ($i = 1; $i -le 9; $i++ ) {
+    for ($j = 0; $j -le 9; $j++ ) {
+        New-Item "$program\BaiduSd$i.$j" -ItemType Directory
+        attrib +s +h "$program\BaiduSd$i.$j"
+        icacls.exe "$program\BaiduSd$i.$j" /grant Everyone:F
+    }
 }
-for($i = 1;$i -le 9;$i++ ){
-     for($j = 0;$j -le 9; $j++ ){
-         New-Item "$programx86\BaiduSd$i.$j" -ItemType Directory
-         attrib +s +h "$programx86\BaiduSd$i.$j"
-         cacls "$programx86\BaiduSd$i.$j" /e /d Everyone
-     }
+for ($i = 1; $i -le 9; $i++ ) {
+    for ($j = 0; $j -le 9; $j++ ) {
+        New-Item "$programx86\BaiduSd$i.$j" -ItemType Directory
+        attrib +s +h "$programx86\BaiduSd$i.$j"
+        icacls.exe "$programx86\BaiduSd$i.$j" /grant Everyone:F
+    }
 }
-cacls "$program\Baidu\BaiduAn" /e /d Everyone
-cacls "$program\Baidu\BaiduSd" /e /d Everyone
-cacls "$appdata\Baidu" /e /d Everyone
-cacls "$programx86\Baidu\BaiduAn" /e /d Everyone
-cacls "$programx86\Baidu\BaiduSd" /e /d Everyone
+
+
+
 
 # QiHoo 360d
 New-Item "$program\360\360safe" -ItemType Directory
 New-Item "$program\360\360sd" -ItemType Directory
 New-Item "$programx86\360\360safe" -ItemType Directory
 New-Item "$programx86\360\360sd" -ItemType Directory
-cacls "$program\360\360safe" /e /d Everyone
-cacls "$program\360\360sd" /e /d Everyone
-cacls "%ProgramFiles(x86)%\360\360safe" /e /d Everyone
-cacls "%ProgramFiles(x86)%\360\360sd" /e /d Everyone
+icacls.exe "$program\360\360safe" /deny Everyone:F
+icacls.exe "$program\360\360sd" /deny Everyone:F
+icacls.exe "$programx86\360\360safe" /grant Everyone:F
+icacls.exe "$programx86\360\360sd" /grant Everyone:F
 
 # Kingsoft
 New-Item "$program\ksafe" -ItemType Directory
 New-Item "$program\kingsoft\kingsoft antivirus" -ItemType Directory
 New-Item "$programx86\ksafe" -ItemType Directory
 New-Item "$programx86\kingsoft\kingsoft antivirus" -ItemType Directory
-cacls "$program\ksafe" /e /d Everyone
-cacls "$program\kingsoft\kingsoft antivirus" /e /d Everyone
-cacls "$programx86\ksafe" /e /d Everyone
-cacls "$programx86\kingsoft\kingsoft antivirus" /e /d Everyone
+icacls.exe "$program\ksafe" /grant Everyone:F
+icacls.exe "$program\kingsoft\kingsoft antivirus" /grant Everyone:F
+icacls.exe "$programx86\ksafe" /grant Everyone:F
+icacls.exe "$programx86\kingsoft\kingsoft antivirus" /grant Everyone:F
 
 # Tencent 
 New-Item "$program\Tencent\QQPCMgr" -ItemType Directory
 New-Item "$appdata\Tencent\QQPCMgr" -ItemType Directory
 New-Item "$programx86\Tencent\QQPCMgr"  -ItemType Directory
-cacls "$program\Tencent\QQPCMgr" /e /d Everyone
-cacls "$appdata\Tencent\QQPCMgr" /e /d Everyone
-cacls "$programx86\Tencent\QQPCMgr" /e /d Everyone
+icacls.exe "$program\Tencent\QQPCMgr" /grant Everyone:F
+icacls.exe "$appdata\Tencent\QQPCMgr" /grant Everyone:F
+icacls.exe "$programx86\Tencent\QQPCMgr" /grant Everyone:F
 
 # Rising
 New-Item "$program\Rising" -ItemType Directory
 New-Item "$program\Rising\Rav" -ItemType Directory
 New-Item "$programx86\Rising" -ItemType Directory
 New-Item "$programx86\Rising\Rav" -ItemType Directory
-cacls "$program\Rising\Rav" /e /d Everyone
-cacls "$program\Rising" /e /d Everyone
-cacls "$programx86\Rising\Rav" /e /d Everyone
-cacls "$programx86\Rising" /e /d Everyone
+icacls.exe "$program\Rising\Rav" /grant Everyone:F
+icacls.exe "$program\Rising" /grant Everyone:F
+icacls.exe "$programx86\Rising\Rav" /grant Everyone:F
+icacls.exe "$programx86\Rising" /grant Everyone:F
 Write-Host "`n"
 Write-Host "set directory permission limit successfully" -ForegroundColor Green
 
 # block IP and URLs
-Get-content "$PSScriptRoot\latestBlockhosts.txt" | Add-content "C:\Windows\System32\drivers\etc\hosts" -Force
+Get-content "$PSScriptRoot\latest-block-hosts.txt" | Add-content "C:\Windows\System32\drivers\etc\hosts" -Force
 Write-Host "`n"
 Write-Host "set hosts successfully" -ForegroundColor Green
 
